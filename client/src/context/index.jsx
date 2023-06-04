8f41d3bea50516a2af67c91a714d7c417daab8ec
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-06-04 17:54:05 +0530

    Contract Update

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -18,14 +18,14 @@ export const StateContextProvider = ({ children }) => {
 
   const publishCampaign = async (form) => {
     try {
-      const data = await createCampaign({ args:[
+      const data = await createCampaign(
         address, // owner
         form.title, // title
         form.description, // description
         form.target,
         new Date(form.deadline).getTime(), // deadline,
         form.image
-    ]})
+    )
       console.log('contract call success', data);
     } catch (error) {
       console.log('contract call failure', error);
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -26,7 +26,6 @@ const CreateCampaign = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log(form);
     checkIfImage(form.image, async (exists) => {
       if(exists) {
         setIsLoading(true)
